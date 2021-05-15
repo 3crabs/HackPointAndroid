4104a32a51eed40dec7a0c176c76b697a8a334b8
Andrey Kataev <katandser96@mail.ru>
2021-05-15 13:09:30 +0700

    add fonts and set manifest name application

--- app/src/androidTest/java/com/threecrabs/hackpoint/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/threecrabs/hackpoint/ExampleInstrumentedTest.kt
@@ -1,8 +1,5 @@
 package com.threecrabs.hackpoint
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,12 +10,12 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.threecrabs.hackpoint", appContext.packageName)
-    }
-}
+//@RunWith(AndroidJUnit4::class)
+//class ExampleInstrumentedTest {
+//    @Test
+//    fun useAppContext() {
+//        // Context of the app under test.
+//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+//        assertEquals("com.threecrabs.hackpoint", appContext.packageName)
+//    }
+//}
--- app/src/main/java/com/threecrabs/hackpoint/App.kt
+++ app/src/main/java/com/threecrabs/hackpoint/App.kt
@@ -1,11 +1,23 @@
 package com.threecrabs.hackpoint
 
 import android.app.Application
+import com.threecrabs.hackpoint.cache.SharedPrefs
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class App: Application() {
 
+    private val appModule = module {
+//        single { ServerRepository(RetrofitServerApi.create(get())) }
+        single { SharedPrefs(get()) }
+    }
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
+        }
     }
 }
